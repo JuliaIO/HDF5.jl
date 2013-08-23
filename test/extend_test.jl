907f54cd3066964ca29c02d14a6b403c245d4e5a
timholy <tim.holy@gmail.com>
2013-08-23 05:00:07 -0500

    Fix typo in test, and put created file in tempdir

--- test/extend_test.jl
+++ test/extend_test.jl
@@ -1,6 +1,8 @@
 using HDF5
 
-fid = h5open("test.h5", "w")
+fn = joinpath(tempdir(),"test.h5")
+
+fid = h5open(fn, "w")
 g = g_create(fid, "shoe")
 d = d_create(g, "foo", datatype(Float64), ((10,20),(100,200)), "chunk", (1,1))
 #println("d is size current $(map(int,HDF5.get_dims(d)[1])) max $(map(int,HDF5.get_dims(d)[2]))")
@@ -35,13 +37,12 @@ b[1:10000] = [1:10000]
 
 close(fid)
 
-println("closing and reopening file")
-fid = h5open("test.h5", "r")
+fid = h5open(fn, "r")
 d_again = fid["shoe/foo"]
 dims, max_dims = HDF5.get_dims(d_again)
 @assert dims==(1,5)
 @assert max_dims == (100,200)
-@assert (sum(d[1,1:5])-sum([1.1231,1.313,5.123,2.231,4.1231])) == 0
+@assert (sum(d_again[1,1:5])-sum([1.1231,1.313,5.123,2.231,4.1231])) == 0
 #println("d is size current $(map(int,HDF5.get_dims(re_d)[1])) max $(map(int,HDF5.get_dims(re_d)[2]))")
 @assert fid["b"][1:10000] == [1:10000]
 b_again = fid["b"]
