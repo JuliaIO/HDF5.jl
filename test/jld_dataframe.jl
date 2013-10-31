c3a9e4626919ffb1f969034e74e330206b37763e
timholy <tim.holy@gmail.com>
2013-10-31 08:25:43 -0500

    Revert "Fix #48"
    This reverts commit 58b8a641096b54b6726abc895645848a07b8b2b5.

--- test/jld_dataframe.jl
+++ test/jld_dataframe.jl
@@ -1,25 +1,22 @@
 using HDF5
 using JLD
+using DataFrames
+
+filename = joinpath(tempdir(), "mydata.jld")
+
+df = DataFrame({[2:6], pi*[1:5]})
+df2 = @DataFrame(a => [1:5], b => pi * [1:5])
+
+file = jldopen(filename, "w")
+write(file, "df", df)
+write(file, "df2", df2)
+close(file)
+
+file = jldopen(filename, "r")
+x = read(file, "df")
+y = read(file, "df2")
+close(file)
+
 using Base.Test
-
-if Pkg.installed("DataFrames") != nothing
-    using DataFrames
-
-    filename = joinpath(tempdir(), "mydata.jld")
-
-    df = DataFrame({[2:6], pi*[1:5]})
-#     df2 = @DataFrame(a => [1:5], b => pi * [1:5])
-
-    file = jldopen(filename, "w")
-    write(file, "df", df)
-#     write(file, "df2", df2)
-    close(file)
-
-    file = jldopen(filename, "r")
-    x = read(file, "df")
-#     y = read(file, "df2")
-    close(file)
-
-    @test all(df .== x)
-#     @test all(df2 .== y)
-end
+@test all(df .== x)
+@test all(df2 .== y)
