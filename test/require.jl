4b0a727b802d6923b02c68a3c441f01e5fc4d6b8
timholy <tim.holy@gmail.com>
2014-05-11 07:55:28 -0500

    Fix problem with test script on Julia 0.2 (conversions-in-constructor issue)

--- test/require.jl
+++ test/require.jl
@@ -5,7 +5,7 @@ using HDF5, JLD
 include("JLDTest.jl")
 
 function create()
-    x = JLDTest(5)
+    x = JLDTest(int16(5))  # int16 makes this work on 0.2
     jldopen("require.jld", "w") do file
         addrequire(file, joinpath(Pkg.dir(), "HDF5", "test", "JLDTest.jl"))
         write(file, "x", x, rootmodule="JLDTemp")
