JuliaIO/HDF5.jl

commit 244e37184073d0a24e7a6a928c9d07f09e9bebe8

Fix writing one-element tuple parameters to JLD files
This wrote SubArrays in a way that they couldn't be read later.

src/jld.jl
@@ -892,7 +892,7 @@ function full_typename(tv::TypeVar)
         "TypeVar(:$(tv.name),$(tv.lb),$(tv.ub))"
     end
 end
-full_typename(jltype::(Type...)) = @sprintf "(%s)" join(map(full_typename, jltype), ",")
+full_typename(jltype::(Type...)) = @sprintf "(%s,)" join(map(full_typename, jltype), ",")
 full_typename(x) = string(x)
 function full_typename(jltype::DataType)
     #tname = "$(jltype.name.module).$(jltype.name)"

test/jld.jl
@@ -63,6 +63,8 @@ bt = reinterpret(MyBT, 55)
 # Symbol arrays (#100)
 sa_asc = [:a, :b]
 sa_utf8 = [:α, :β]
+# SubArray (to test tuple type params)
+subarray = sub([1:5], 1:5)
 
 iseq(x,y) = isequal(x,y)
 iseq(x::MyStruct, y::MyStruct) = (x.len == y.len && x.data == y.data)
@@ -161,6 +163,7 @@ fid = jldopen(fn, "w")
 @write fid bt
 @write fid sa_asc
 @write fid sa_utf8
+@write fid subarray
 # Make sure we can create groups (i.e., use HDF5 features)
 g = g_create(fid, "mygroup")
 i = 7
@@ -224,6 +227,7 @@ for mmap = (true, false)
     @check fidr bt
     @check fidr sa_asc
     @check fidr sa_utf8
+    @check fidr subarray
     
     x1 = read(fidr, "group1/x")
     @assert x1 == {1}

commit 5c7eb7d88e80085615c39d73b6e433032811ee0c

Fix writing empty tuple parameters to JLD files
I broke this in 244e37184073d0a24e7a6a928c9d07f09e9bebe8

src/jld.jl
@@ -894,7 +894,8 @@ function full_typename(tv::TypeVar)
         "TypeVar(:$(tv.name),$(tv.lb),$(tv.ub))"
     end
 end
-full_typename(jltype::(Type...)) = @sprintf "(%s,)" join(map(full_typename, jltype), ",")
+full_typename(jltype::(Type...)) = length(jltype) == 1 ? @sprintf("(%s,)", full_typename(jltype[1])) :
+                                   @sprintf("(%s)", join(map(full_typename, jltype), ","))
 full_typename(x) = string(x)
 function full_typename(jltype::DataType)
     #tname = "$(jltype.name.module).$(jltype.name)"
@@ -983,7 +984,6 @@ macro load(filename, vars...)
             end
         end
         return Expr(:block, 
-                    Expr(:global, vars...),
                     Expr(:try,  Expr(:block, readexprs...), false, false,
                          :(close($f))),
                     Symbol[v.args[1] for v in vars]) # "unescape" vars
@@ -994,7 +994,6 @@ macro load(filename, vars...)
         end
         return Expr(:block, 
                     :(local f = jldopen($(esc(filename)))),
-                    Expr(:global, map(esc, vars)...),
                     Expr(:try,  Expr(:block, readexprs...), false, false,
                          :(close(f))),
                     Symbol[v for v in vars]) # vars is a tuple

test/jld.jl
@@ -65,6 +65,8 @@ sa_asc = [:a, :b]
 sa_utf8 = [:α, :β]
 # SubArray (to test tuple type params)
 subarray = sub([1:5], 1:5)
+# Array of empty tuples (to test tuple type params)
+arr_empty_tuple = ()[]
 
 iseq(x,y) = isequal(x,y)
 iseq(x::MyStruct, y::MyStruct) = (x.len == y.len && x.data == y.data)
@@ -164,6 +166,7 @@ fid = jldopen(fn, "w")
 @write fid sa_asc
 @write fid sa_utf8
 @write fid subarray
+@write fid arr_empty_tuple
 # Make sure we can create groups (i.e., use HDF5 features)
 g = g_create(fid, "mygroup")
 i = 7
@@ -228,6 +231,7 @@ for mmap = (true, false)
     @check fidr sa_asc
     @check fidr sa_utf8
     @check fidr subarray
+    @check fidr arr_empty_tuple
     
     x1 = read(fidr, "group1/x")
     @assert x1 == {1}