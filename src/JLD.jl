ce2c44f4ab9d936334c744633c8dada455bd657a
Simon Kornblith <simon@simonster.com>
2014-08-14 18:48:18 -0400

    Fix some type instability in read_array

--- src/JLD.jl
+++ src/JLD.jl
@@ -340,8 +340,8 @@ end
 read_array{T<:HDF5BitsKind}(obj::JldDataset, dtype::HDF5Datatype, ::Type{Array{T}}, dspace_id::HDF5.Hid) =
     obj.file.mmaparrays && HDF5.iscontiguous(obj.plain) ? readmmap(obj.plain, Array{T}) : read(obj.plain, Array{T})
 function read_array(obj::JldDataset, dtype::HDF5Datatype, T::Type, dspace_id::HDF5.Hid)
-    dims = map(int, HDF5.h5s_get_simple_extent_dims(dspace_id)[1])
-    n = prod(dims)
+    dims = map(int, HDF5.h5s_get_simple_extent_dims(dspace_id)[1])::(Int...)
+    n = prod(dims)::Int
     h5sz = sizeof(dtype)
     out = Array(T, dims)
 
