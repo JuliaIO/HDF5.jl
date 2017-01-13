ccbf38dac12d1015aa6755d7d74bedfc869c0a99
Mus M <mus-m@outlook.com>
2017-01-12 23:08:49 -0500

    Fix macro hygiene bug in test files and deprecated string method and other bugfix

--- src/plain.jl
+++ src/plain.jl
@@ -1008,7 +1008,7 @@ name(attr::HDF5Attribute) = h5a_get_name(attr.id)
             resize!(buf, len+10)
             len = h5g_get_objname_by_idx(x.id, i - 1, buf, length(buf))
         end
-        res[i] = @compat String(buf[1:len])
+        res[i] = String(buf[1:len])
     end
     res
 end
@@ -1021,7 +1021,7 @@ function names(x::HDF5Attributes)
         len = h5a_get_name_by_idx(x.parent.id, ".", H5_INDEX_NAME, H5_ITER_INC, i-1, "", 0, H5P_DEFAULT)
         buf = Array(UInt8, len+1)
         len = h5a_get_name_by_idx(x.parent.id, ".", H5_INDEX_NAME, H5_ITER_INC, i-1, buf, len+1, H5P_DEFAULT)
-        res[i] = @compat String(buf[1:len])
+        res[i] = String(buf[1:len])
     end
     res
 end
@@ -1296,7 +1296,7 @@ function read{S<:String}(obj::DatasetOrAttribute, ::Type{S})
             pad = h5t_get_strpad(objtype.id)
             buf = Array(UInt8, n)
             readarray(obj, objtype.id, buf)
-			pbuf = @compat String(buf)
+			pbuf = String(buf)
             ret = unpad(pbuf, pad)
         end
     finally
@@ -1383,7 +1383,7 @@ end
 # Read compound type
 @compat function read(obj::HDF5Dataset, ::Union{Type{Array{HDF5Compound}},Type{HDF5Compound}})
     t = datatype(obj)
-    local sz = 0, n, membername, membertype, memberoffset, memberfiletype
+    local sz = 0; local n; local membername; local membertype; local memberoffset; local memberfiletype
     try
         n = h5t_get_nmembers(t.id)
         memberfiletype = Array(HDF5Datatype, n)
@@ -2145,19 +2145,19 @@ function h5a_get_name(attr_id::Hid)
     len = h5a_get_name(attr_id, 0, C_NULL) # order of args differs from {f,i}_get_name
     buf = Array(UInt8, len+1)
     h5a_get_name(attr_id, len+1, buf)
-    @compat unsafe_string(buf[1:len])
+    String(buf[1:len])
 end
 function h5f_get_name(loc_id::Hid)
     len = h5f_get_name(loc_id, C_NULL, 0)
     buf = Array(UInt8, len+1)
     h5f_get_name(loc_id, buf, len+1)
-    @compat String(buf[1:len])
+    String(buf[1:len])
 end
 function h5i_get_name(loc_id::Hid)
     len = h5i_get_name(loc_id, C_NULL, 0)
     buf = Array(UInt8, len+1)
     h5i_get_name(loc_id, buf, len+1)
-    @compat String(buf[1:len])
+    String(buf[1:len])
 end
 function h5l_get_info(link_loc_id::Hid, link_name::String, lapl_id::Hid)
     info = Array(H5LInfo, 1)
