73d35ee198875c677e92dc1e56bea77e4b8215ce
Mus M <mus-m@outlook.com>
2017-03-31 11:09:57 -0400

    Remove Compat for Filesystem which was necessary for dropped 0.4 compatibility

--- src/HDF5.jl
+++ src/HDF5.jl
@@ -603,10 +603,10 @@ function h5rewrite(f::Function, filename::AbstractString, args...)
 
     try
         val = h5open(f, tmppath, "w", args...)
-        Compat.Filesystem.rename(tmppath, filename)
+        Base.rename(tmppath, filename)
         return val
     catch
-        Compat.Filesystem.unlink(tmppath)
+        Base.unlink(tmppath)
         rethrow()
     end
 end
