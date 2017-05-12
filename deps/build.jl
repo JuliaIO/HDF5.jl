a857a1c61bdb63064317e90ff58f48713403b370
ggggggggg <ggggggggg@users.noreply.github.com>
2017-05-11 23:47:35 -0600

    validate libhdf5 version in build.jl (#396)
    Validate libhdf5 version in build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -2,8 +2,22 @@ using BinDeps
 
 @BinDeps.setup
 
+# https://support.hdfgroup.org/HDF5/ lists "Current Releases"
+# make sure we have one of those
+const MINVERSION = v"1.8.0"
+function h5_get_libversion(lib, handle)
+    sym = Libdl.dlsym_e(handle, "H5get_libversion")
+    majnum, minnum, relnum = Ref{Cuint}(), Ref{Cuint}(), Ref{Cuint}()
+    status = ccall(sym, Cint, (Ptr{Cuint}, Ptr{Cuint}, Ptr{Cuint}), majnum, minnum, relnum)
+    status < 0 && error("Error getting HDF5 library version")
+    VersionNumber(majnum[], minnum[], relnum[])
+end
+compatible_version(lib, handle) = h5_get_libversion(lib, handle) >= MINVERSION
+
 if is_linux()
-    hdf5 = library_dependency("libhdf5", aliases = ["libhdf5", "libhdf5_serial", "libhdf5_serial.so.10", "libhdf5_openmpi", "libhdf5_mpich"])
+    hdf5 = library_dependency("libhdf5",
+        aliases = ["libhdf5", "libhdf5_serial", "libhdf5_serial.so.10", "libhdf5_openmpi", "libhdf5_mpich"],
+        validate=compatible_version)
     provides(AptGet, "hdf5-tools", hdf5)
     provides(Pacman, "hdf5", hdf5)
     provides(Yum, "hdf5", hdf5)
@@ -11,14 +25,14 @@ end
 
 if is_windows()
     using WinRPM
-    hdf5 = library_dependency("libhdf5")
-    provides(WinRPM.RPM, "hdf5", hdf5, os = :Windows )
+    hdf5 = library_dependency("libhdf5", validate=compatible_version)
+    provides(WinRPM.RPM, "hdf5", hdf5, os=:Windows)
 end
 
 if is_apple()
     using Homebrew
-    hdf5 = library_dependency("libhdf5")
-    provides(Homebrew.HB, "homebrew/science/hdf5", hdf5, os = :Darwin )
+    hdf5 = library_dependency("libhdf5", validate=compatible_version)
+    provides(Homebrew.HB, "homebrew/science/hdf5", hdf5, os=:Darwin)
 end
 
 provides(Sources, URI("https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.0-patch1/src/hdf5-1.10.0-patch1.tar.gz"), hdf5)
