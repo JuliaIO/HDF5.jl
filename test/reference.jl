e9fb1c2fe259fefdb158ee52845bc3964f2f8f43
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2020-08-25 15:25:54 -0700

    add separate tests for HDF5ReferenceObj

---
+++ test/reference.jl
@@ -0,0 +1,17 @@
+using Random, Test, HDF5
+
+@testset "reference" begin
+  data = rand(100)
+
+  fn = "output.h5"
+  f = h5open(fn, "w")
+  f["data"] = data
+  f["data_ref"] = HDF5.HDF5ReferenceObj(f, "data")
+  close(f)
+
+  f = h5open(fn, "r")
+  ref = read(f["data_ref"])
+  dset = f[ref]
+  data_read = read(dset)
+  @test data == data_read
+end
