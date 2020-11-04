90ed84bf51e3eaa285a634da03ce5ce186ddae82
Mustafa M <mus-m@outlook.com>
2020-11-04 02:03:06 -0500

    Wrap all tests in a testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ using Pkg
 
 println("HDF5 version ", HDF5.h5_get_libversion())
 
+@testset "HDF5.jl" begin
+
 include("plain.jl")
 include("compound.jl")
 include("custom.jl")
@@ -22,4 +24,6 @@ try
     # basic MPI tests, for actual parallel tests we need to run in MPI mode
     include("mpio.jl")
 catch
+end
+    
 end
