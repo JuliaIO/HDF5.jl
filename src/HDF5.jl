5a76e1b3fe1ab0138b204518130dbf288cf1139f
Mustafa Mohamad <mus-m@outlook.com>
2020-11-17 16:10:33 -0500

    Refactor h5open method

--- src/HDF5.jl
+++ src/HDF5.jl
@@ -444,7 +444,7 @@ function h5open(filename::AbstractString, mode::AbstractString = "r"; swmr::Bool
     # (Set this first, so that the user-passed properties can overwrite this.)
     apl[:fclose_degree] = H5F_CLOSE_STRONG
     cpl = create_property(H5P_FILE_CREATE; pv...) # file create property list
-    modes =
+    rd, wr, cr, tr, ff =
         mode == "r"  ? (true,  false, false, false, false) :
         mode == "r+" ? (true,  true,  false, false, true ) :
         mode == "cw" ? (false, true,  true,  false, true ) :
@@ -452,7 +452,37 @@ function h5open(filename::AbstractString, mode::AbstractString = "r"; swmr::Bool
         # mode == "w+" ? (true,  true,  true,  true,  false) :
         # mode == "a"  ? (true,  true,  true,  true,  true ) :
         error("invalid open mode: ", mode)
-    return _h5open(filename, modes..., cpl, apl; swmr=swmr)
+    if ff && !wr
+        error("HDF5 does not support appending without writing")
+    end
+
+    close_apl = false
+    if apl == DEFAULT_PROPERTIES
+        apl = create_property(H5P_FILE_ACCESS)
+        close_apl = true
+        # With garbage collection, the other modes don't make sense
+        apl[:fclose_degree] = H5F_CLOSE_STRONG
+    end
+    if cr && (tr || !isfile(filename))
+        flag = swmr ? H5F_ACC_TRUNC|H5F_ACC_SWMR_WRITE : H5F_ACC_TRUNC
+        fid = h5f_create(filename, flag, cpl, apl)
+    else
+        if !h5f_is_hdf5(filename)
+            error("This does not appear to be an HDF5 file")
+        end
+        if wr
+            flag = swmr ? H5F_ACC_RDWR|H5F_ACC_SWMR_WRITE : H5F_ACC_RDWR
+        else
+            flag = swmr ? H5F_ACC_RDONLY|H5F_ACC_SWMR_READ : H5F_ACC_RDONLY
+        end
+        fid = h5f_open(filename, flag, apl)
+    end
+    if close_apl
+        # Close properties manually to avoid errors when the file is
+        # closed before the properties are gc'ed
+        close(apl)
+    end
+    File(fid, filename)
 end
 
 """
@@ -1289,7 +1319,6 @@ do_reclaim(::Type{T}) where T <: Union{Cstring,VariableArray} = true
 
 Base.read(attr::Attributes, name::AbstractString) = read_attribute(attr.parent, name)
 
-# Reading with mmap
 function iscontiguous(obj::Dataset)
     prop = h5d_get_create_plist(checkvalid(obj))
     try
@@ -1299,6 +1328,7 @@ function iscontiguous(obj::Dataset)
     end
 end
 
+# Reading with mmap
 ismmappable(::Type{<:ScalarType}) = true
 ismmappable(::Type) = false
 ismmappable(obj::Dataset, ::Type{T}) where {T} = ismmappable(T) && iscontiguous(obj)
