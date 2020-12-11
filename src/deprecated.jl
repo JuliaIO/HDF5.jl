808608ca97aa00dc34348ffbe5737adebfab809a
Mustafa Mohamad <mus-m@outlook.com>
2020-12-11 13:01:53 -0500

    Typo

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -446,6 +446,4 @@ end
 
 ### Changed in PR#776
 @deprecate create_dataset(parent::Union{File,Group}, path::AbstractString, dtype::Datatype, dspace::Dataspace,
-lcpl::Properties, dcpl::Properties,
-dapl::Properties, dxpl::Properties) HDF5.Dataset(
-        HDF5.h5d_create(parent, path, dtype, dspace, lcpl, dcpl, dapl), HDF5.file(parent), dxpl)) false
+    lcpl::Properties, dcpl::Properties, dapl::Properties, dxpl::Properties) HDF5.Dataset(HDF5.h5d_create(parent, path, dtype, dspace, lcpl, dcpl, dapl), HDF5.file(parent), dxpl) false
