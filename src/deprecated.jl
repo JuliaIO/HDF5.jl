5cbf782da7c0d883496231f033ee37708d2dbea1
Mustafa M <mus-m@outlook.com>
2020-12-31 13:29:58 -0500

    Update deprecated.jl

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -4,6 +4,9 @@ import Base: @deprecate, @deprecate_binding, depwarn
 ### v0.15 deprecations
 ###
 
+export exists
+function exists end
+
 ### Changed in PR#776
 @deprecate create_dataset(parent::Union{File,Group}, path::AbstractString, dtype::Datatype, dspace::Dataspace,
     lcpl::Properties, dcpl::Properties, dapl::Properties, dxpl::Properties) HDF5.Dataset(HDF5.h5d_create(parent, path, dtype, dspace, lcpl, dcpl, dapl), HDF5.file(parent), dxpl) false
