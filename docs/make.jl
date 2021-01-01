b7cc2b991c47736e7463ce1bfd15a9587f93a406
Mustafa M <mus-m@outlook.com>
2021-01-01 12:57:52 -0500

    Remove doc logo and tweak CI (#793)
    * Remove doc logo and tweak CI

--- docs/make.jl
+++ docs/make.jl
@@ -4,14 +4,14 @@ using MPI  # needed to generate docs for parallel HDF5 API
 
 
 makedocs(;
+    sitename="HDF5.jl",
     modules=[HDF5],
     authors="Mustafa Mohamad <mus-m@outlook.com> and contributors",
-    repo="https://github.com/JuliaIO/HDF5.jl/blob/{commit}{path}#L{line}",
-    sitename="HDF5.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://JuliaIO.github.io/HDF5.jl",
         assets=String[],
+        sidebar_sitename=false,
     ),
     pages=[
         "Home" => "index.md",
