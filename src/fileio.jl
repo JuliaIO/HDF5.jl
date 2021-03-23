6889a5df96c9f2d9b7b6ab0ea9d916a42c372c96
Mustafa M <mus-m@outlook.com>
2021-03-23 18:06:25 -0400

    Update fileio.jl

--- src/fileio.jl
+++ src/fileio.jl
@@ -13,20 +13,20 @@ function loadtodict!(d::Dict, g::Union{File,Group}, prefix::String="")
 end
 
 # load with just a filename returns a flat dictionary containing all the variables
-function load(f::FileIO.File{FileIO.format"HDF5"}; kwargs...)
+function fileio_load(f::FileIO.File{FileIO.format"HDF5"}; kwargs...)
     h5open(FileIO.filename(f), "r"; kwargs...) do file
         loadtodict!(Dict{String,Any}(), file)
     end
 end
 
 # When called with explicitly requested variable names, return each one
-function load(f::FileIO.File{FileIO.format"HDF5"}, varname::AbstractString; kwargs...)
+function fileio_load(f::FileIO.File{FileIO.format"HDF5"}, varname::AbstractString; kwargs...)
     h5open(FileIO.filename(f), "r"; kwargs...) do file
         read(file, varname)
     end
 end
 
-function load(f::FileIO.File{FileIO.format"HDF5"}, varnames::AbstractString...; kwargs...)
+function fileio_load(f::FileIO.File{FileIO.format"HDF5"}, varnames::AbstractString...; kwargs...)
     h5open(FileIO.filename(f), "r"; kwargs...) do file
         map(var -> read(file, var), varnames)
     end
