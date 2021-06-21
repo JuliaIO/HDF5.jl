59f8a8c00bc4c0d4fef7618413d508d88403d534
Mustafa Mohamad <mus-m@outlook.com>
2021-06-21 11:53:25 -0400

    Implement FileIO saver

--- src/fileio.jl
+++ src/fileio.jl
@@ -19,7 +19,7 @@ function fileio_load(f::FileIO.File{FileIO.format"HDF5"}; kwargs...)
     end
 end
 
-# When called with explicitly requested variable names, return each one
+# when called with explicitly requested variable names, return each one
 function fileio_load(f::FileIO.File{FileIO.format"HDF5"}, varname::AbstractString; kwargs...)
     h5open(FileIO.filename(f), "r"; kwargs...) do file
         read(file, varname)
@@ -29,5 +29,17 @@ end
 function fileio_load(f::FileIO.File{FileIO.format"HDF5"}, varnames::AbstractString...; kwargs...)
     h5open(FileIO.filename(f), "r"; kwargs...) do file
         map(var -> read(file, var), varnames)
+    end
+end
+
+# save all the key-value pairs in the dict as top-level variables
+function fileio_save(f::FileIO.File{FileIO.format"HDF5"}, dict::AbstractDict; kwargs...)
+    h5open(FileIO.filename(f), "w"; kwargs...) do file
+        for (k,v) in dict
+            if !isa(k, AbstractString)
+                throw(ArgumentError("keys must be strings (the names of variables), got $k"))
+            end
+            write(file, String(k), v)
+        end
     end
 end
