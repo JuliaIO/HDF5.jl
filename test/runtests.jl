33f5136ef811a849fa6426dba9eeb5c521c607d2
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2021-12-13 14:34:46 -0500

    Add test debug Cherry-pick 740983ed4bc99ec56ba09aa1f6915d789066cec5

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,24 +4,44 @@ using Pkg
 
 @info "libhdf5 v$(HDF5.API.h5_get_libversion())"
 
+# To debug HDF5.jl tests, uncomment the next line
+# ENV["JULIA_DEBUG"] = "Main"
+
 @testset "HDF5.jl" begin
 
+@debug "plain"
 include("plain.jl")
+@debug "compound"
 include("compound.jl")
+@debug "custom"
 include("custom.jl")
+@debug "reference"
 include("reference.jl")
+@debug "dataspace"
 include("dataspace.jl")
+@debug "hyperslab"
 include("hyperslab.jl")
+@debug "readremote"
 include("readremote.jl")
+@debug "extend_test"
 include("extend_test.jl")
+@debug "gc"
 include("gc.jl")
+@debug "external"
 include("external.jl")
+@debug "swmr"
 include("swmr.jl")
+@debug "mmap"
 include("mmap.jl")
+@debug "properties"
 include("properties.jl")
+@debug "table"
 include("table.jl")
+@debug "filter"
 include("filter.jl")
+@debug "chunkstorage"
 include("chunkstorage.jl")
+@debug "fileio"
 include("fileio.jl")
 
 using MPI
