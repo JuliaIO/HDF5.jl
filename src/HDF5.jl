832d85321aace45e76c4a884bf42989abd021761
KristofferC <kcarlsson89@gmail.com>
2022-01-25 15:37:43 +0100

    remove eval in front of requires blocks

--- src/HDF5.jl
+++ src/HDF5.jl
@@ -1611,8 +1611,8 @@ function __init__()
     ASCII_ATTRIBUTE_PROPERTIES.char_encoding = :ascii
     UTF8_ATTRIBUTE_PROPERTIES.char_encoding = :utf8
 
-    @require FileIO="5789e2e9-d7fb-5bc7-8068-2c6fae9b9549" @eval include("fileio.jl")
-    @require H5Zblosc="c8ec2601-a99c-407f-b158-e79c03c2f5f7" @eval begin
+    @require FileIO="5789e2e9-d7fb-5bc7-8068-2c6fae9b9549" include("fileio.jl")
+    @require H5Zblosc="c8ec2601-a99c-407f-b158-e79c03c2f5f7" begin
         set_blosc!(p::Properties, val::Bool) = val && push!(Filters.FilterPipeline(p), H5Zblosc.BloscFilter())
         set_blosc!(p::Properties, level::Integer) = push!(Filters.FilterPipeline(p), H5Zblosc.BloscFilter(level=level))
     end
