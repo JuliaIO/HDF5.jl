c053526a299f3e5456218b1604fb314215783b94
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2022-02-09 03:42:58 -0500

    Add error handling

--- src/windows.jl
+++ src/windows.jl
@@ -1,7 +1,12 @@
 # Windows specific methods
 
 function win_setenv(key, value)
-    ccall(:_wputenv_s, Cint, (Cwstring,Cwstring), key, value)
+    errno = ccall(:_wputenv_s, Cint, (Cwstring,Cwstring), key, value)
+    if errno != 0
+        # Expect errno == EINVAL (22)
+        error("win_setenv failed with key=\"$key\" and value=\"$value\"")
+    end
+    nothing
 end
 function win_sync_hdf5_plugin_path()
     # Fix https://github.com/JuliaIO/HDF5.jl/issues/905
@@ -11,7 +16,11 @@ function win_sync_hdf5_env()
     # Issue #905, JuliaLang/julia#44054
     for (key, value) in ENV
         if startswith(key, "HDF5")
-            win_setenv(key, value)
+            try
+                win_setenv(key, value)
+            catch
+                @warn "Could not set environmental for HDF5" key value
+            end
         end
     end
 end
