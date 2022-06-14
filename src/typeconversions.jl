565fcaa769b93ab3970538117e8af95b56343636
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2022-06-14 05:55:28 -0400

    Move some types back to typeconversions.jl

--- src/typeconversions.jl
+++ src/typeconversions.jl
@@ -47,6 +47,56 @@ function datatype(str::VLen{C}) where {C<:CharType}
     Datatype(API.h5t_vlen_create(type_id))
 end
 
+# Opaque types
+struct Opaque
+  data
+  tag::String
+end
+
+# An empty array type
+struct EmptyArray{T} <: AbstractArray{T,0} end
+# Required AbstractArray interface
+Base.size(::EmptyArray) = ()
+Base.IndexStyle(::Type{<:EmptyArray}) = IndexLinear()
+Base.getindex(::EmptyArray, ::Int) = error("cannot index an `EmptyArray`")
+Base.setindex!(::EmptyArray, v, ::Int) = error("cannot assign to an `EmptyArray`")
+# Optional interface
+Base.similar(::EmptyArray{T}) where {T} = EmptyArray{T}()
+Base.similar(::EmptyArray, ::Type{S}) where {S} = EmptyArray{S}()
+Base.similar(::EmptyArray, ::Type{S}, dims::Dims) where {S} = Array{S}(undef, dims)
+# Override behavior for 0-dimensional Array
+Base.length(::EmptyArray) = 0
+# Required to avoid indexing during printing
+Base.show(io::IO, E::EmptyArray) = print(io, typeof(E), "()")
+Base.show(io::IO, ::MIME"text/plain", E::EmptyArray) = show(io, E)
+# FIXME: Concatenation doesn't work for this type (it's treated as a length-1 array like
+# Base's 0-dimensional arrays), so just forceably abort.
+Base.cat_size(::EmptyArray) = error("concatenation of HDF5.EmptyArray is unsupported")
+Base.cat_size(::EmptyArray, d) = error("concatenation of HDF5.EmptyArray is unsupported")
+
+# Stub types to encode fixed-size arrays for API.H5T_ARRAY
+struct FixedArray{T,D,L}
+    data::NTuple{L,T}
+end
+Base.size(::Type{FixedArray{T,D,L}}) where {T,D,L} = D
+Base.size(x::FixedArray) = size(typeof(x))
+Base.eltype(::Type{FixedArray{T,D,L}}) where {T,D,L} = T
+Base.eltype(x::FixedArray) = eltype(typeof(x))
+
+struct FixedString{N,PAD}
+    data::NTuple{N,UInt8}
+end
+Base.length(::Type{FixedString{N,PAD}}) where {N,PAD} = N
+Base.length(str::FixedString) = length(typeof(str))
+pad(::Type{FixedString{N,PAD}}) where {N,PAD} = PAD
+pad(x::T) where {T<:FixedString} = pad(T)
+
+struct VariableArray{T}
+    len::Csize_t
+    p::Ptr{Cvoid}
+end
+Base.eltype(::Type{VariableArray{T}}) where T = T
+
 ## Conversion between Julia types and HDF5 atomic types
 hdf5_type_id(::Type{Bool})      = API.H5T_NATIVE_B8
 hdf5_type_id(::Type{Int8})      = API.H5T_NATIVE_INT8
