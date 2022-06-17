c18b9614ffd95c37633e909121bfdbb2806e9504
Simon Byrne <simonbyrne@gmail.com>
2022-06-16 19:10:06 -0700

    move file-related functions to new file

--- src/HDF5.jl
+++ src/HDF5.jl
@@ -47,6 +47,7 @@ const ORDER = Ref(API.H5_ITER_INC)
 
 include("properties.jl")
 include("types.jl")
+include("file.jl")
 include("typeconversions.jl")
 include("dataspaces.jl")
 include("datasets.jl")
@@ -56,107 +57,6 @@ include("references.jl")
 include("show.jl")
 
 ### High-level interface ###
-
-"""
-    h5open(filename::AbstractString, mode::AbstractString="r"; swmr=false, pv...)
-
-Open or create an HDF5 file where `mode` is one of:
- - "r"  read only
- - "r+" read and write
- - "cw" read and write, create file if not existing, do not truncate
- - "w"  read and write, create a new file (destroys any existing contents)
-
-Pass `swmr=true` to enable (Single Writer Multiple Reader) SWMR write access for "w" and
-"r+", or SWMR read access for "r".
-
-Properties can be specified as keywords for [`FileAccessProperties`](@ref) and [`FileCreateProperties`](@ref).
-
-Also the keywords `fapl` and `fcpl` can be used to provide default instances of these property lists. Property
-lists passed in via keyword will be closed. This is useful to set properties not currently defined by HDF5.jl.
-
-Note that `h5open` uses `fclose_degree = :strong` by default, but this can be overriden by the `fapl` keyword.
-"""
-function h5open(filename::AbstractString, mode::AbstractString, fapl::FileAccessProperties, fcpl::FileCreateProperties=FileCreateProperties(); swmr::Bool = false)
-    rd, wr, cr, tr, ff =
-        mode == "r"  ? (true,  false, false, false, false) :
-        mode == "r+" ? (true,  true,  false, false, true ) :
-        mode == "cw" ? (false, true,  true,  false, true ) :
-        mode == "w"  ? (false, true,  true,  true,  false) :
-        # mode == "w+" ? (true,  true,  true,  true,  false) :
-        # mode == "a"  ? (true,  true,  true,  true,  true ) :
-        error("invalid open mode: ", mode)
-    if ff && !wr
-        error("HDF5 does not support appending without writing")
-    end
-
-    if cr && (tr || !isfile(filename))
-        flag = swmr ? API.H5F_ACC_TRUNC|API.H5F_ACC_SWMR_WRITE : API.H5F_ACC_TRUNC
-        fid = API.h5f_create(filename, flag, fcpl, fapl)
-    else
-        ishdf5(filename) || error("unable to determine if $filename is accessible in the HDF5 format (file may not exist)")
-        if wr
-            flag = swmr ? API.H5F_ACC_RDWR|API.H5F_ACC_SWMR_WRITE : API.H5F_ACC_RDWR
-        else
-            flag = swmr ? API.H5F_ACC_RDONLY|API.H5F_ACC_SWMR_READ : API.H5F_ACC_RDONLY
-        end
-        fid = API.h5f_open(filename, flag, fapl)
-    end
-    return File(fid, filename)
-end
-
-
-function h5open(filename::AbstractString, mode::AbstractString = "r";
-    swmr::Bool = false,
-    # With garbage collection, the other modes don't make sense
-    fapl = FileAccessProperties(; fclose_degree = :strong),
-    fcpl = FileCreateProperties(),
-    pv...
-)
-    try
-        pv = setproperties!(fapl, fcpl; pv...)
-        isempty(pv) || error("invalid keyword options $pv")
-        file = h5open(filename, mode, fapl, fcpl; swmr=swmr)
-        return file
-    finally
-        close(fapl)
-        close(fcpl)
-    end
-end
-
-
-"""
-    function h5open(f::Function, args...; swmr=false, pv...)
-
-Apply the function f to the result of `h5open(args...; kwargs...)` and close the resulting
-`HDF5.File` upon completion. For example with a `do` block:
-
-    h5open("foo.h5","w") do h5
-        h5["foo"]=[1,2,3]
-    end
-
-"""
-function h5open(f::Function, args...; swmr=false, pv...)
-    file = h5open(args...; swmr=swmr, pv...)
-    try
-        f(file)
-    finally
-        close(file)
-    end
-end
-
-function h5rewrite(f::Function, filename::AbstractString, args...)
-    tmppath,tmpio = mktemp(dirname(filename))
-    close(tmpio)
-
-    try
-        val = h5open(f, tmppath, "w", args...)
-        Base.Filesystem.rename(tmppath, filename)
-        return val
-    catch
-        Base.Filesystem.unlink(tmppath)
-        rethrow()
-    end
-end
 
 function h5write(filename, name::AbstractString, data; pv...)
     file = h5open(filename, "cw"; pv...)
@@ -220,23 +120,6 @@ checkvalid(obj) = isvalid(obj) ? obj : error("File or object has been closed")
 
 # Close functions
 
-# Close functions that should try calling close regardless
-function Base.close(obj::File)
-    if obj.id != -1
-        API.h5f_close(obj)
-        obj.id = -1
-    end
-    nothing
-end
-
-"""
-    isopen(obj::HDF5.File)
-
-Returns `true` if `obj` has not been closed, `false` if it has been closed.
-"""
-Base.isopen(obj::File) = obj.id != -1
-
-
 # Close functions that should first check that the file is still open. The common case is a
 # file that has been closed with CLOSE_STRONG but there are still finalizers that have not run
 # for the datasets, etc, in the file.
@@ -261,27 +144,6 @@ function Base.close(obj::Datatype)
     nothing
 end
 
-
-"""
-    ishdf5(name::AbstractString)
-
-Returns `true` if the file specified by `name` is in the HDF5 format, and `false` otherwise.
-"""
-function ishdf5(name::AbstractString)
-    isfile(name) || return false # fastpath in case the file is non-existant
-    # TODO: v1.12 use the more robust API.h5f_is_accesible
-    try
-        # docs falsely claim API.h5f_is_hdf5 doesn't error, but it does
-        return API.h5f_is_hdf5(name)
-    catch
-        return false
-    end
-end
-
-# Extract the file
-file(f::File) = f
-file(o::Union{Object,Attribute}) = o.file
-fd(obj::Object) = API.h5i_get_file_id(checkvalid(obj))
 
 # Flush buffers
 Base.flush(f::Union{Object,Attribute,Datatype,File}, scope = API.H5F_SCOPE_GLOBAL) = API.h5f_flush(checkvalid(f), scope)
@@ -431,7 +293,6 @@ Base.isempty(x::Union{Group,File}) = length(x) == 0
 Base.eltype(dset::Union{Dataset,Attribute}) = get_jl_type(dset)
 
 # filename and name
-filename(obj::Union{File,Group,Dataset,Attribute,Datatype}) = API.h5f_get_name(checkvalid(obj))
 name(obj::Union{File,Group,Dataset,Datatype}) = API.h5i_get_name(checkvalid(obj))
 function Base.keys(x::Union{Group,File})
     checkvalid(x)
@@ -472,14 +333,6 @@ datatype(dt::Datatype) = dt
 
 Base.sizeof(dtype::Datatype) = Int(API.h5t_get_size(dtype))
 
-
-"""
-    start_swmr_write(h5::HDF5.File)
-
-Start Single Reader Multiple Writer (SWMR) writing mode.
-See [SWMR documentation](https://portal.hdfgroup.org/display/HDF5/Single+Writer+Multiple+Reader++-+SWMR).
-"""
-start_swmr_write(h5::File) = API.h5f_start_swmr_write(h5)
 
 
 # Clean up string buffer according to padding mode
