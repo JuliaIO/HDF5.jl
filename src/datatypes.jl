c469063a833b0616ad36df9f69858005e1a25412
Simon Byrne <simonbyrne@gmail.com>
2022-06-22 21:12:13 -0700

    move datatype functions to new file (#975)

---
+++ src/datatypes.jl
@@ -0,0 +1,32 @@
+function Base.close(obj::Datatype)
+    if obj.toclose && obj.id != -1
+        if (!isdefined(obj, :file) || obj.file.id != -1) && isvalid(obj)
+            API.h5o_close(obj)
+        end
+        obj.id = -1
+    end
+    nothing
+end
+
+# The datatype of a Datatype is the Datatype
+datatype(dt::Datatype) = dt
+
+
+open_datatype(parent::Union{File,Group}, name::AbstractString, tapl::DatatypeAccessProperties=DatatypeAccessProperties()) =
+    Datatype(API.h5t_open(checkvalid(parent), name, tapl), file(parent))
+
+# Note that H5Tcreate is very different; H5Tcommit is the analog of these others
+create_datatype(class_id, sz) = Datatype(API.h5t_create(class_id, sz))
+
+function commit_datatype(parent::Union{File,Group}, path::AbstractString, dtype::Datatype,
+                  lcpl::LinkCreateProperties=LinkCreateProperties(),
+                  tcpl::DatatypeCreateProperties=DatatypeCreateProperties(),
+                  tapl::DatatypeAccessProperties=DatatypeAccessProperties())
+    lcpl.char_encoding = cset(typeof(path))
+    API.h5t_commit(checkvalid(parent), path, dtype, lcpl, tcpl, tapl)
+    dtype.file = file(parent)
+    return dtype
+end
+
+
+Base.sizeof(dtype::Datatype) = Int(API.h5t_get_size(dtype))
