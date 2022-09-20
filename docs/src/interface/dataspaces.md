0120cb2d32fc8d57727b3dcb17860c63c00a0cd3
Simon Byrne <simonbyrne@gmail.com>
2022-09-20 08:08:05 -0700

    add BlockRange object to represent a dimension of a hyperslab (#1011)
    * add BlockRange object to represent dimension of hyperslab
    
    * add range conversions, more tests, and docs
    
    * make stride positive to handle 0 length ranges
    
    * remove api function
    
    * Update src/dataspaces.jl
    
    Co-authored-by: Mark Kittisopikul <mkitti@users.noreply.github.com>
    
    * add tests for conversions
    
    * run formatter
    
    * Support additional operators
    
    Co-authored-by: Mark Kittisopikul <mkitti@users.noreply.github.com>
    
    * fixes and tests for printing
    
    * fix printing, add tests
    
    Co-authored-by: Mark Kittisopikul <mkitti@users.noreply.github.com>

--- docs/src/interface/dataspaces.md
+++ docs/src/interface/dataspaces.md
@@ -4,4 +4,7 @@
 HDF5.Dataspace
 dataspace
 HDF5.isnull
+HDF5.BlockRange
+HDF5.select_hyperslab!
+HDF5.get_regular_hyperslab
 ```
--- test/hyperslab.jl
+++ test/hyperslab.jl
@@ -1,5 +1,53 @@
 using Random, Test, HDF5
 
+@testset "BlockRange" begin
+    br = HDF5.BlockRange(2)
+    @test length(br) == 1
+    @test range(br) === 2:2
+    @test convert(AbstractRange, br) === 2:2
+    @test convert(UnitRange, br) === 2:2
+    @test convert(StepRange, br) === 2:1:2
+    @test repr(br) == "HDF5.BlockRange(2)"
+    @test repr(br; context=:compact => true) == "2"
+
+    br = HDF5.BlockRange(Base.OneTo(3))
+    @test length(br) == 3
+    @test range(br) == 1:3
+    @test convert(AbstractRange, br) === 1:3
+    @test convert(UnitRange, br) === 1:3
+    @test convert(StepRange, br) === 1:1:3
+    @test repr(br) == "HDF5.BlockRange(1:3)"
+    @test repr(br; context=:compact => true) == "1:3"
+
+    br = HDF5.BlockRange(2:7)
+    @test length(br) == 6
+    @test range(br) == 2:7
+    @test convert(AbstractRange, br) === 2:7
+    @test convert(UnitRange, br) === 2:7
+    @test convert(StepRange, br) === 2:1:7
+    @test repr(br) == "HDF5.BlockRange(2:7)"
+    @test repr(br; context=:compact => true) == "2:7"
+
+    br = HDF5.BlockRange(1:2:7)
+    @test length(br) == 4
+    @test range(br) == 1:2:7
+    @test convert(AbstractRange, br) === 1:2:7
+    @test_throws Exception convert(UnitRange, br)
+    @test convert(StepRange, br) === 1:2:7
+    @test repr(br) == "HDF5.BlockRange(1:2:7)"
+    @test repr(br; context=:compact => true) == "1:2:7"
+
+    br = HDF5.BlockRange(; start=2, stride=8, count=3, block=2)
+    @test length(br) == 6
+    @test_throws Exception range(br)
+    @test_throws Exception convert(AbstractRange, br)
+    @test_throws Exception convert(UnitRange, br)
+    @test_throws Exception convert(StepRange, br)
+    @test repr(br) == "HDF5.BlockRange(start=2, stride=8, count=3, block=2)"
+    @test repr(br; context=:compact => true) ==
+        "BlockRange(start=2, stride=8, count=3, block=2)"
+end
+
 @testset "hyperslab" begin
     N = 10
     v = [randstring(rand(5:10)) for i in 1:N, j in 1:N]
@@ -22,5 +70,23 @@ using Random, Test, HDF5
 
         indices = (1:2:10, 1:3:10)
         @test dset[indices...] == v[indices...]
+
+        indices = (
+            HDF5.BlockRange(1:2; stride=4, count=2), HDF5.BlockRange(1; stride=5, count=2)
+        )
+        @test dset[indices...] == vcat(v[1:2, 1:5:6], v[5:6, 1:5:6])
+    end
+end
+
+@testset "read 0-length arrays: issue #859" begin
+    fname = tempname()
+    dsetname = "foo"
+
+    h5open(fname, "w") do fid
+        create_dataset(fid, dsetname, datatype(Float32), ((0,), (-1,)); chunk=(100,))
+    end
+
+    h5open(fname, "r") do fid
+        fid[dsetname][:]
     end
 end
