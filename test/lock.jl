6bbd35c20393133cf07efff6e0053e6a235a2465
Mark Kittisopikul <markkitt@gmail.com>
2022-11-08 01:01:28 -0500

    Add the test lock file

---
+++ test/lock.jl
@@ -0,0 +1,23 @@
+using HDF5
+if VERSION >= v"1.6"
+    using Preferences
+end
+using Test
+
+@testset "API Lock Preference" begin
+    prev_use_api_lock = HDF5.API.get_use_api_lock()
+    @test prev_use_api_lock isa Bool
+    @static if VERSION >= v"1.6"
+        HDF5.API.set_use_api_lock!(true)
+        @test load_preference(HDF5, "use_api_lock") == true
+        HDF5.API.set_use_api_lock!(false)
+        @test load_preference(HDF5, "use_api_lock") == false
+        HDF5.API.set_use_api_lock!(prev_use_api_lock)
+        @test load_preference(HDF5, "use_api_lock") == prev_use_api_lock
+        delete_preferences!(HDF5, "use_api_lock"; force=true)
+        @test load_preference(HDF5, "use_api_lock") === nothing
+    else
+        @test prev_use_api_lock
+        @test_throws ErrorException HDF5.API.set_use_api_lock(true)
+    end
+end
