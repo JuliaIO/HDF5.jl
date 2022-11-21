098c4eb619552d4b393aaee156d9eaf981ec5d39
nhz2 <nhz2@cornell.edu>
2022-11-21 16:34:01 -0500

    simplify

--- src/attributes.jl
+++ src/attributes.jl
@@ -250,17 +250,8 @@ end
 function Base.setindex!(attrdict::AttributeDict, val, name::AbstractString)
     if haskey(attrdict, name)
         # in case of an error, we write first to a temporary, then rename
-        max_tries = 100 # number of times to try making a new name
-        _name = nothing
-        for i in 1:max_tries
-            _name = "-><7;*<_NY"*bytes2hex(rand(UInt8, 16))
-            if haskey(attrdict, _name)
-                _name = nothing
-            else
-                break
-            end
-        end
-        isnothing(_name) && error("tempname: max_tries exhausted")
+        _name = "-><7;*<_NY"*bytes2hex(rand(UInt8, 16))
+        haskey(attrdict, _name) && error("`attrdict` has the temp name somehow")
         try
             write_attribute(attrdict.parent, _name, val)
             delete_attribute(attrdict.parent, name)
