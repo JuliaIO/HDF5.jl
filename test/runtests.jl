92e38837f6882ef055838d28e3462ed14a63b502
Mark Kittisopikul <mkitti@users.noreply.github.com>
2022-11-22 15:27:22 -0500

    Don't use testset verbose=true for Julia 1.3 compat

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,7 +17,7 @@ end
 # To debug HDF5.jl tests, uncomment the next line
 # ENV["JULIA_DEBUG"] = "Main"
 
-@testset verbose=true "HDF5.jl" begin
+@testset "HDF5.jl" begin
     @debug "plain"
     include("plain.jl")
     @debug "api"
