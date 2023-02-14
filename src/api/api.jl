f8de5d44242b59dd0850f2e51a7ed41c27217f2e
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2023-02-14 09:21:46 -0600

    Always load even if deps aren't satisfied

--- src/api/api.jl
+++ src/api/api.jl
@@ -7,7 +7,7 @@ const depsfile = joinpath(@__DIR__, "..", "..", "deps", "deps.jl")
 if isfile(depsfile)
     include(depsfile)
 else
-    error(
+    @error(
         "HDF5 is not properly installed. Please run Pkg.build(\"HDF5\") ",
         "and restart Julia."
     )
