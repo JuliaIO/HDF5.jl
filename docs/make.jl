e82e18ad0ff22d08fe111e12026875ade1b0d385
Joshua Lampert <joshua.lampert@tuhh.de>
2023-09-10 13:15:59 +0200

    remove unused not_low_level_api

--- docs/make.jl
+++ docs/make.jl
@@ -6,19 +6,6 @@ using H5Zlz4
 using H5Zzstd
 using MPI  # needed to generate docs for parallel HDF5 API
 
-# Used in index.md to filter the autodocs list
-not_low_level_api(m::Method) = !endswith(String(m.file), "src/api.jl")
-not_low_level_api(f::Function) = all(not_low_level_api, methods(f))
-not_low_level_api(o) = true
-
-# defined on separate page
-not_low_level_api(::Type{<:HDF5.Properties}) = false
-
-# Manually-defined low-level API (in source file src/api/helpers.jl)
-not_low_level_api(::typeof(HDF5.API.h5p_get_class_name)) = false
-not_low_level_api(::typeof(HDF5.API.h5t_get_member_name)) = false
-not_low_level_api(::typeof(HDF5.API.h5t_get_tag)) = false
-
 makedocs(;
     sitename="HDF5.jl",
     modules=[HDF5, H5Zblosc, H5Zbzip2, H5Zlz4, H5Zzstd],
