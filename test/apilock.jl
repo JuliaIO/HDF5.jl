b7039568bfebf55b1cf496432e26526b94006375
Lorenzo Van Munoz <lorenzo@vanmunoz.com>
2024-11-20 11:19:00 -0500

    format

--- test/apilock.jl
+++ test/apilock.jl
@@ -1,7 +1,9 @@
 using Pkg, Test
 @testset "issue1179" begin
     @test try
-        run(`$(Base.julia_cmd()) --project=$(dirname(Pkg.project().path)) -t 6 $(joinpath(@__DIR__, "issue1179.jl"))`)
+        run(
+            `$(Base.julia_cmd()) --project=$(dirname(Pkg.project().path)) -t 6 $(joinpath(@__DIR__, "issue1179.jl"))`
+        )
         true
     catch err
         @info err
--- test/issue1179.jl
+++ test/issue1179.jl
@@ -7,7 +7,7 @@ Threads.@threads for i in 1:Threads.nthreads()
     h5open("file$i.hdf5", "w") do h5
         d = create_dataset(h5, "rand", T, sizes)
         for n in 1:sizes[3]
-            d[:,:,n] = rand(T, sizes[1:2])
+            d[:, :, n] = rand(T, sizes[1:2])
         end
     end
 end
